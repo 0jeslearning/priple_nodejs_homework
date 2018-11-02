9217d3801c104b740e973e6c7ad48dca7680e955
Ojes Otrebla <ojesotrebla@yahoo.com>
2018-11-02 07:20:40 -0300

    Get path and trimmed

--- index.js
+++ index.js
@@ -3,15 +3,28 @@
  * 
  */
 
-//Dependecies
+// Dependecies
 const http = require('http');
+const url = require('url');
 
-//The server should response to all requests with a string
+// The server should response to all requests with a string
 const server = http.createServer(function(req, res) {
+    // Get the URL and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    console.log(path);
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end('Hello World\n');
+
+    // Log the request path
+    console.log(`Request received on path ${trimmedPath}`);
 })
 
-//Start the server, and have it listen on port 3000
+// Start the server, and have it listen on port 3000
 server.listen(3000, function() {
     console.log("The server is listening on port 3000 now");
 })
